44651d59b6c78f31e3c42c135ce172a0a9698d7e
Eduardo <ebfjunior@gmail.com>
2018-01-09 22:48:37 -0200

    separa configuracao do express

--- cdc.js
+++ cdc.js
@@ -1,9 +1,6 @@
-const express = require('express')
-const app = express()
+const app = require('./config-express')
 
 const home = require('./routes/home')(app)
 const produtos = require('./routes/produtos')(app)
-
-app.set('view engine', 'ejs')
 
 app.listen(3000, () => { console.log('subiu') })
---
+++ config-express.js
@@ -0,0 +1,5 @@
+const express = require('express')
+const app = express()
+app.set('view engine', 'ejs')
+
+module.exports = app
