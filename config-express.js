50fe1b8ecf6b64c09baf523137be4d8a1eeaf8fb
Eduardo <ebfjunior@gmail.com>
2018-01-11 22:51:49 -0200

    carrega rotas com express load

--- config-express.js
+++ config-express.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const expressValidator = require('express-validator')
+const load = require('express-load')
 
 app.set('view engine', 'ejs')
 app.use(express.static('./public'))
@@ -9,8 +10,7 @@ app.use(express.static('./public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(expressValidator())
 
-require('./routes/home')(app)
-require('./routes/produtos')(app)
+load('routes').into(app)
 
 app.use((request, response, next) => {
     response.status(404).send(`Erro 404 da pagina ${request.originalUrl}`)
